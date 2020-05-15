49bad4df408a8c1b79b2cdca31c283fbf4575389
djuro77 <djuro77@protonmail.com>
2020-05-15 22:13:08 +0200

    Button Colors and Stay on top added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter.colorchooser import askcolor
+
 from tkinter import *
 import pyperclip
 import pickle
@@ -14,12 +16,13 @@ buttons = []
 
 # if app does not start with at least 1 button there will be an error from
 # menu_button_delete menu widget that requires at least one argument
-buttons.append({"category":"Temp", "label":"Welcome", "content":"Hello"})
+buttons.append({"category":"Temp", "label":"Welcome", "content":"Hello", "color":None})
 
 # variables to determine button location in gui grid
 next_column = {"OneLiners":1, "Emails":1, "QuickNotes":1, "Temp":1}
 category_row = {"OneLiners":1, "Emails":2, "QuickNotes":3, "Temp":4}
 
+button_color = 'SystemButtonFace'
 ## Helper Functions START ##
 def pickle_out():
     """Exports buttons list to .pkl file"""
@@ -39,13 +42,13 @@ def clipboard_command(value_to_return):
     return lambda: pyperclip.copy(value_to_return)
 
 
-def new_button(category=None, command=None, text=None, row=0, column=0):
+def new_button(category=None, command=None, text=None, row=0, column=0, color=None):
     """Adds new button to the frame_buttons - displays button"""
     global next_column
     if next_column[category] > 30:
         print("Max number of Buttons")
         return
-    Button(frame_buttons, text=text, command=command, width=10).grid(column=column, row=row)
+    Button(frame_buttons, text=text, command=command, width=10, background=color).grid(column=column, row=row)
     next_column[category] += 1
 
 
@@ -72,7 +75,9 @@ def build_buttons():
     global next_column
     next_column = {"OneLiners":1, "Emails":1, "QuickNotes":1, "Temp":1} # reset start position
     for b in buttons:
-        new_button(category=b['category'], command=clipboard_command(b['content']), text=b['label'], row=category_row[b['category']], column=next_column[b['category']])
+        new_button(category=b['category'], command=clipboard_command(b['content']),\
+         text=b['label'], row=category_row[b['category']], column=next_column[b['category']],\
+         color=b['color'])
 
 
 def update_delete_menu():
@@ -83,11 +88,14 @@ def update_delete_menu():
         menu.add_command(label=label, command=lambda v=label: var_button_delete.set(v))
 
 
-## Button actions - to be passed as Button command= kwarg  ##
+## following func are Button actions - to be passed as a value to Button command= kwarg  ##
 
 def command_new_button():
     label = entry_new_label.get()
-    if label in [button['label'] for button in buttons]:
+    if label == "":
+        messagebox.showinfo("Failed to create a new Button", "You must provide a unique Label")
+        return
+    elif label in [button['label'] for button in buttons]:
         messagebox.showinfo("Failed to create a new Button", "Label '{}' already in use!".format(label))
         return
 
@@ -96,11 +104,17 @@ def command_new_button():
     content = entry_new_content.get()
     command = clipboard_command(content)
     row = category_row[category]
-    new_button(category=category, command=command, text=label, row=row, column=next_column[category])
-    buttons.append({"category":category, "label":label, "content":content})
+    global button_color
+    color = button_color
+    new_button(category=category, command=command, text=label, row=row, column=next_column[category], color=color)
+    buttons.append({"category":category, "label":label, "content":content, "color":color})
+
+    # updating things
     update_delete_menu()
     entry_new_label.delete(0, END)
     entry_new_content.delete(0, END)
+    button_button_color.config(background='SystemButtonFace')
+    button_color = 'SystemButtonFace'
 
 def command_import():
     import_filename = filedialog.askopenfilename(initialdir = "export/",title = "Select file",filetypes = (("pkl files","*.pkl"),("all files","*.*")))
@@ -121,6 +135,20 @@ def command_delete_button():
 
     # update delete menu
     update_delete_menu()
+
+
+def command_always_on_top():
+    if var_always_on_top.get() == 1:
+        root_window.wm_attributes("-topmost", 1)
+    else:
+        root_window.wm_attributes("-topmost", 0)
+
+
+def get_button_color():
+    global button_color
+    button_color = askcolor(title="Button Color")[1]
+    button_button_color.config(background=button_color)
+
 
 ## START GUI ##
 
@@ -150,18 +178,25 @@ var_categories.set(categories[0])
 menu_categories = OptionMenu(frame_add_new, var_categories, *categories)
 menu_categories.grid(column=6, row=0)
 
-Button(frame_add_new, text="ADD", command=command_new_button).grid(column=7, row=0)
+button_button_color = Button(frame_add_new, text="Choose color", command=get_button_color)
+button_button_color.grid(column=7, row=0)
 
-Button(frame_add_new, text="Delete =>", command=command_delete_button).grid(column=8, row=0)
+Button(frame_add_new, text="ADD", command=command_new_button).grid(column=8, row=0)
+
+Button(frame_add_new, text="Delete =>", command=command_delete_button).grid(column=9, row=0)
 
 var_button_delete = StringVar(frame_add_new)
 buttons_labels = [button['label'] for button in buttons]
 menu_button_delete = OptionMenu(frame_add_new, var_button_delete, *buttons_labels)
-menu_button_delete.grid(column=9, row=0)
+var_button_delete.set(buttons_labels[0])
+menu_button_delete.grid(column=10, row=0)
 
-Button(frame_add_new, text="Import", command=command_import).grid(column=10, row=0)
-Button(frame_add_new, text="Export", command=pickle_out).grid(column=11, row=0)
+Button(frame_add_new, text="Import", command=command_import).grid(column=11, row=0)
+Button(frame_add_new, text="Export", command=pickle_out).grid(column=12, row=0)
 
+var_always_on_top = IntVar()
+ckbox_always_on_top = Checkbutton(frame_add_new, text="Always on top", variable=var_always_on_top, command=command_always_on_top)
+ckbox_always_on_top.grid(column=13, row=0)
 
 build_buttons_frame()
 build_buttons()
