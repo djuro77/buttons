d1284dd3922b11866d7332a484a8321aced5af64
djuro77 <44552409+djuro77@users.noreply.github.com>
2020-12-24 10:38:42 +0100

    build, requirements, recompiled

--- buttons_venv/Scripts/Activate.ps1
+++ buttons_venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\ivand\Desktop\Buttons\buttons_venv"
+$env:VIRTUAL_ENV="C:\Users\ivand\Desktop\Projects\Buttons\buttons_venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
